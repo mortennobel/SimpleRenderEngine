238c6f1a8ef676944f49f04dc6e69534847b00e3
Morten Nobel-Jørgensen <morten@nobel-joergensen.com>
2016-09-12 06:19:21 -0700

    Fix glm library issue

--- SRE/Export.hpp
+++ SRE/Export.hpp
@@ -1,7 +1,14 @@
 #pragma once
 
+#include "glm/glm.hpp"
+
 #ifdef _WIN32
 #define DllExport __declspec(dllexport)
+template struct __declspec(dllexport) glm::tvec4<float, glm::precision(0)>;
+template struct __declspec(dllexport) glm::tvec3<float, glm::precision(0)>;
+template struct __declspec(dllexport) glm::tvec2<float, glm::precision(0)>;
+template struct __declspec(dllexport) glm::tmat4x4<float, glm::precision(0)>;
+template struct __declspec(dllexport) glm::tmat3x3<float, glm::precision(0)>;
 #else
 #define DllExport 
 #endif // _WIN32
