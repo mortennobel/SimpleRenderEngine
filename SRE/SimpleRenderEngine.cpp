bb2bf4b33f5d769c93258e38295010ddc363f844
Alex Busnita <alexandru.busnita@yahoo.ro>
2016-09-12 21:18:35 +0200

    Added force dedicated GPU for windows users, if the system does not have either Optimus or AMD Power the above lines have no effect.

--- SRE/SimpleRenderEngine.cpp
+++ SRE/SimpleRenderEngine.cpp
@@ -10,6 +10,11 @@
 #if defined(_WIN32)
 #   define GLEW_STATIC
 #   include <GL/glew.h>
+extern "C"
+{
+	__declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
 #elif defined __linux__
 #   include <GL/glew.h>
 #else
