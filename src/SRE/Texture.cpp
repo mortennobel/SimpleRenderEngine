59dfa89736453cfd8e342892dbf42811a8a9cbad
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2016-10-28 11:34:55 +0200

    Clean up GL header

--- src/SRE/Texture.cpp
+++ src/SRE/Texture.cpp
@@ -1,13 +1,6 @@
 #include "SRE/Texture.hpp"
 
-#if defined(_WIN32)
-#   define GLEW_STATIC
-#   include <GL/glew.h>
-#elif defined __linux__
-#   include <GL/glew.h>
-#else
-#   include <OpenGL/gl3.h>
-#endif
+#include "SRE/SREGL.hpp"
 #include <stdio.h>
 #include <SDL_surface.h>
 
