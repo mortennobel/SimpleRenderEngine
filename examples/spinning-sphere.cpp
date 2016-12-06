7fb51bb9f497e7e71c279c95425de2e84c71d866
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2016-12-06 19:42:43 +0100

    Version 0.2.9 Fixed issues with specular highlight

--- examples/spinning-sphere.cpp
+++ examples/spinning-sphere.cpp
@@ -9,13 +9,33 @@
 #include "SRE/Shader.hpp"
 #define SDL_MAIN_HANDLED
 #include "SDL.h"
+#include <imgui.h>
+#include "SRE/imgui_sre.hpp"
 
 #include <glm/glm.hpp>
 
 #include <glm/gtx/euler_angles.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <SRE/Debug.hpp>
 
 using namespace SRE;
+
+void drawCross(glm::vec3 p, float size = 0.3f){
+    SRE::Debug::drawLine(p-glm::vec3{size,0,0}, p+glm::vec3{size,0,0});
+    SRE::Debug::drawLine(p-glm::vec3{0,size,0}, p+glm::vec3{0,size,0});
+    SRE::Debug::drawLine(p-glm::vec3{0,0,size}, p+glm::vec3{0,0,size});
+}
+
+void drawLight(Light& l, float size){
+    if (l.lightType == LightType::Point || l.lightType == LightType::Directional){
+        SRE::Debug::setColor({0,0,0,1});
+        drawCross(l.position, size);
+    }
+    if (l.lightType == LightType::Directional){
+        SRE::Debug::setColor({1,1,0,1});
+        SRE::Debug::drawLine(l.position, l.position - l.direction*size*2.0f);
+    }
+}
 
 int main() {
     std::cout << "Spinning sphere"<<std::endl;
@@ -46,30 +66,110 @@ int main() {
     }
 
     SimpleRenderEngine r{window};
-
-    r.getCamera()->lookAt({0,0,3},{0,0,0},{0,1,0});
+    ImGui_SRE_Init(window);
+    glm::vec3 eye{0,0,5};
+    glm::vec3 at{0,0,0};
+    glm::vec3 up{0,1,0};
+    r.getCamera()->lookAt(eye,at, up);
     r.getCamera()->setPerspectiveProjection(60,640,480,0.1f,100);
     Shader* shader = Shader::getStandard();
-    shader->set("specularity",20.0f);
+    float specularity = 20;
+    glm::vec4 color {1,1,1,1};
+    shader->set("specularity", 20.0f);
+    shader->set("color", color);
     Mesh* mesh = Mesh::createSphere();
 
-    bool point = true;
-    if (point){
-        r.setLight(0, Light(LightType::Point,{0, 2,1},{0,0,0},{1,0,0},10));
-        r.setLight(1, Light(LightType::Point,{2, 0,1},{0,0,0},{0,1,0},10));
-        r.setLight(2, Light(LightType::Point,{0,-2,1},{0,0,0},{0,0,1},10));
-        r.setLight(3, Light(LightType::Point,{-2,0,1},{0,0,0},{1,1,1},10));
-    } else {
-        r.setLight(0, Light(LightType::Directional,{0,0,0},{0,1,0},{1,0,0},10));
-        r.setLight(1, Light(LightType::Directional,{0,0,0},{1,0,0},{0,1,0},10));
-        r.setLight(2, Light(LightType::Directional,{0,0,0},{0,-1,0},{0,0,1},10));
-        r.setLight(3, Light(LightType::Directional,{0,0,0},{-1,0,0},{1,1,1},10));
-    }
+    Light lights[] = {
+            Light(LightType::Point,{0, 2,1},{0,0,0},{1,0,0},10),
+            Light(LightType::Unused,{2, 0,1},{0,0,0},{0,1,0},10),
+            Light(LightType::Unused,{0,-2,1},{0,0,0},{0,0,1},10),
+            Light(LightType::Unused,{-2,0,1},{0,0,0},{1,1,1},10),
+    };
 
-    float duration = 10000;
-    for (float i=0;i<duration ;i+=16){
+    bool debugLight = true;
+    bool animatedLight = true;
+    bool animatedCamera = true;
+    float debugLightSize = 0.2;
+    bool quit = false;
+    SDL_Event e;
+    float time = 0;
+    while (!quit){
+        //Handle events on queue
+        while( SDL_PollEvent( &e ) != 0 )
+        {
+            ImGui_SRE_ProcessEvent(&e);
+            if (e.type == SDL_QUIT)
+                quit = true;
+        }
         r.clearScreen({1,0,0,1});
-        r.draw(mesh, glm::eulerAngleY(glm::radians(360 * i / duration)), shader);
+        drawCross({2,2,2});
+        drawCross({-2,-2,-2});
+        r.draw(mesh, glm::eulerAngleY(time), shader);
+        time += 0.016f;
+
+        ImGui_SRE_NewFrame(window);
+        std::string labels[] = {
+                "Light 1",
+                "Light 2",
+                "Light 3",
+                "Light 4"
+        };
+
+        ImGui::DragFloat3("Camera",&eye.x);
+        ImGui::Checkbox("AnimatedLight",&animatedLight);
+        ImGui::Checkbox("AnimatedCamera",&animatedCamera);
+        if (animatedCamera){
+            eye = {
+                sin(time*-0.2)*5.0f,
+                        sin(time*-0.4)*0.5f,
+                        cos(time*-0.2)*5.0f,
+            };
+        }
+        r.getCamera()->lookAt(eye,at, up);
+        if (animatedLight){
+            lights[0].position = {
+                    sin(time)*1.5f,
+                    sin(time*2)*0.5f,
+                    cos(time)*1.5f,
+            };
+        }
+        ImGui::Checkbox("DebugLight",&debugLight);
+        if (debugLight){
+            ImGui::DragFloat("DebugLightSize", &debugLightSize,0.1f,0,3);
+        }
+        // Show Label (with invisible window)
+        for (int i=0;i<4;i++){
+            if (debugLight){
+                drawLight(lights[i],debugLightSize);
+            }
+            if (ImGui::TreeNode(labels[i].c_str())){
+                auto lightType = (int)lights[i].lightType;
+                ImGui::RadioButton("Point", &lightType, 0); ImGui::SameLine();
+                ImGui::RadioButton("Directional", &lightType, 1); ImGui::SameLine();
+                ImGui::RadioButton("Unused", &lightType, 2);
+                lights[i].lightType = (LightType)lightType;
+
+                ImGui::ColorEdit3("Color", &(lights[i].color.x));
+                ImGui::DragFloat3("Position", &(lights[i].position.x));
+                ImGui::DragFloat3("Direction", &(lights[i].direction.x));
+                ImGui::DragFloat("Range", &(lights[i].range),1,0,30);
+
+
+                ImGui::TreePop();
+            }
+            r.setLight(i, lights[i]);
+        }
+
+        if (ImGui::TreeNode("Material")){
+            ImGui::DragFloat("Specularity", &specularity,1,0,200);
+            shader->set("specularity", specularity);
+            ImGui::ColorEdit3("Color", &(color.x));
+            shader->set("color", color);
+            ImGui::TreePop();
+        }
+
+        ImGui::Render();
+
         r.swapWindow();
         SDL_Delay(16);
     }
--- src/SRE/Shader.cpp
+++ src/SRE/Shader.cpp
@@ -662,30 +662,32 @@ vec3 computeLight(){
         vec3 lightDirection;
         float att = 1.0;
         if (isDirectional){
-            lightDirection = lightPosType[i].xyz;
+            lightDirection = normalize(lightPosType[i].xyz);
         } else if (isPoint) {
             vec3 lightVector = lightPosType[i].xyz - vEyePos;
             float lightVectorLength = length(lightVector);
             float lightRange = lightColorRange[i].w;
-            lightDirection = lightVector/lightVectorLength;
+            lightDirection = lightVector / lightVectorLength;
             if (lightRange <= 0.0){
                 att = 1.0;
             } else if (lightVectorLength >= lightRange){
                 att = 0.0;
             } else {
-                att = pow(1.0-lightVectorLength/lightRange,1.5); // non physical range based attenuation
+                att = pow(1.0 - lightVectorLength / lightRange,1.5); // non physical range based attenuation
             }
         } else {
             continue;
         }
-        vec3 H = normalize(lightDirection - vEyePos);
+
         // diffuse light
         float thisDiffuse = max(0.0,dot(lightDirection, normal));
         if (thisDiffuse > 0.0){
            lightColor += (att * diffuseFrac * thisDiffuse) * lightColorRange[i].xyz;
         }
+
         // specular light
         if (specularity > 0.0){
+            vec3 H = normalize(lightDirection - normalize(vEyePos));
             float nDotHV = dot(normal, H);
             if (nDotHV > 0.0){
                 float pf = pow(nDotHV, specularity);
