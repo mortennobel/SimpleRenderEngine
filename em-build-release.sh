fce1f694673b85fb6d008cf22da62b83b6431866
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2018-05-13 14:11:51 +0200

    Update location of examples

--- em-build-release.sh
+++ em-build-release.sh
@@ -33,7 +33,7 @@ emcc -Iinclude src/imgui/imgui.cpp \
                src/sre/Skybox.cpp \
                src/sre/impl/UniformSet.cpp \
                examples/10_skybox-example.cpp \
-               -O2 -std=c++14 -s USE_WEBGL2=1 -s FORCE_FILESYSTEM=1 -s TOTAL_MEMORY=33554432 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' --embed-file examples_data/cube-negx.png --embed-file examples_data/cube-negy.png --embed-file examples_data/cube-negz.png --embed-file examples_data/cube-posx.png --embed-file examples_data/cube-posy.png --embed-file examples_data/cube-posz.png -s USE_SDL=2 -o html_examples/10_skybox-example.html
+               -O2 -std=c++14 -s USE_WEBGL2=1 -s FORCE_FILESYSTEM=1 -s TOTAL_MEMORY=33554432 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' --embed-file examples_data/cube-negx.png --embed-file examples_data/cube-negy.png --embed-file examples_data/cube-negz.png --embed-file examples_data/cube-posx.png --embed-file examples_data/cube-posy.png --embed-file examples_data/cube-posz.png -s USE_SDL=2 -o docs/examples/10_skybox-example.html
 
 
 emcc -Iinclude src/imgui/imgui.cpp \
@@ -61,7 +61,7 @@ emcc -Iinclude src/imgui/imgui.cpp \
                src/sre/Skybox.cpp \
                src/sre/impl/UniformSet.cpp \
                examples/12_render-to-texture.cpp \
-               -O2 -std=c++14 -s USE_WEBGL2=1 -s FORCE_FILESYSTEM=1 -s TOTAL_MEMORY=33554432 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='[]'  -s USE_SDL=2 -o html_examples/12_render-to-texture.html
+               -O2 -std=c++14 -s USE_WEBGL2=1 -s FORCE_FILESYSTEM=1 -s TOTAL_MEMORY=33554432 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='[]'  -s USE_SDL=2 -o docs/examples/12_render-to-texture.html
 
 emcc -Iinclude src/imgui/imgui.cpp \
                src/imgui/imgui_draw.cpp \
@@ -88,7 +88,7 @@ emcc -Iinclude src/imgui/imgui.cpp \
                src/sre/Skybox.cpp \
                src/sre/impl/UniformSet.cpp \
                examples/07_matcap.cpp \
-               -O2 -std=c++14 -s USE_WEBGL2=1 -s FORCE_FILESYSTEM=1 -s TOTAL_MEMORY=33554432 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' --embed-file examples_data/suzanne.obj --embed-file examples_data/matcap_00001.png --embed-file examples_data/matcap_00002.png --embed-file examples_data/matcap_00003.png --embed-file examples_data/matcap_00004.png -s USE_SDL=2 -o html_examples/07_matcap.html
+               -O2 -std=c++14 -s USE_WEBGL2=1 -s FORCE_FILESYSTEM=1 -s TOTAL_MEMORY=33554432 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' --embed-file examples_data/suzanne.obj --embed-file examples_data/matcap_00001.png --embed-file examples_data/matcap_00002.png --embed-file examples_data/matcap_00003.png --embed-file examples_data/matcap_00004.png -s USE_SDL=2 -o docs/examples/07_matcap.html
 
 emcc -Iinclude src/imgui/imgui.cpp \
                src/imgui/imgui_draw.cpp \
@@ -115,7 +115,7 @@ emcc -Iinclude src/imgui/imgui.cpp \
                src/sre/Skybox.cpp \
                src/sre/impl/UniformSet.cpp \
                examples/09_picking.cpp \
-               -O2 -std=c++14 -s USE_WEBGL2=1 -s FORCE_FILESYSTEM=1 -s TOTAL_MEMORY=33554432 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='[]' -s USE_SDL=2 -o html_examples/09_picking.html
+               -O2 -std=c++14 -s USE_WEBGL2=1 -s FORCE_FILESYSTEM=1 -s TOTAL_MEMORY=33554432 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='[]' -s USE_SDL=2 -o docs/examples/09_picking.html
 
 emcc -Iinclude src/imgui/imgui.cpp \
                src/imgui/imgui_draw.cpp \
@@ -142,7 +142,7 @@ emcc -Iinclude src/imgui/imgui.cpp \
                src/sre/Skybox.cpp \
                src/sre/impl/UniformSet.cpp \
                examples/15_cloth_simulation.cpp \
-               -O2 -std=c++14 -s USE_WEBGL2=1 -s FORCE_FILESYSTEM=1 -s TOTAL_MEMORY=33554432 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='[]' -s USE_SDL=2 -o html_examples/15_cloth_simulation.html
+               -O2 -std=c++14 -s USE_WEBGL2=1 -s FORCE_FILESYSTEM=1 -s TOTAL_MEMORY=33554432 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='[]' -s USE_SDL=2 -o docs/examples/15_cloth_simulation.html
 
 emcc -Iinclude src/imgui/imgui.cpp \
                src/imgui/imgui_draw.cpp \
@@ -169,4 +169,4 @@ emcc -Iinclude src/imgui/imgui.cpp \
                src/sre/Skybox.cpp \
                src/sre/impl/UniformSet.cpp \
                examples/16_shadows.cpp \
-               -O2 -std=c++14 -s USE_WEBGL2=1 -s FORCE_FILESYSTEM=1 -s TOTAL_MEMORY=33554432 --embed-file examples_data/suzanne.obj -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='[]' -s USE_SDL=2 -o html_examples/16_shadows.html
+               -O2 -std=c++14 -s USE_WEBGL2=1 -s FORCE_FILESYSTEM=1 -s TOTAL_MEMORY=33554432 --embed-file examples_data/suzanne.obj -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='[]' -s USE_SDL=2 -o docs/examples/16_shadows.html
