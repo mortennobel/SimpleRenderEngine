d8444b4d479965281e38cbebbcf1541cbf476593
Morten Nobel-Jørgensen <morten@nobel-joergensen.com>
2018-06-04 12:14:09 +0200

    Fix windows compile issue

--- include/sre/impl/Export.hpp
+++ include/sre/impl/Export.hpp
@@ -15,12 +15,6 @@
 #define DllExport __declspec(dllexport)
 #define EXPIMP_TEMPLATE
 
-template struct __declspec(dllexport) glm::tvec4<float, glm::precision(0)>;
-template struct __declspec(dllexport) glm::tvec3<float, glm::precision(0)>;
-template struct __declspec(dllexport) glm::tvec2<float, glm::precision(0)>;
-template struct __declspec(dllexport) glm::tmat4x4<float, glm::precision(0)>;
-template struct __declspec(dllexport) glm::tmat3x3<float, glm::precision(0)>;
-
 // https://support.microsoft.com/en-us/kb/168958
 //disable warnings on 255 char debug symbols
 #pragma warning (disable : 4786)
@@ -29,12 +23,6 @@ template struct __declspec(dllexport) glm::tmat3x3<float, glm::precision(0)>;
 
 #include <vector>
 
-EXPIMP_TEMPLATE template class DECLSPECIFIER std::vector<int>;
-EXPIMP_TEMPLATE template class DECLSPECIFIER std::vector<float>;
-EXPIMP_TEMPLATE template class DECLSPECIFIER std::vector<uint16_t>;
-EXPIMP_TEMPLATE template class DECLSPECIFIER std::vector<glm::vec2>;
-EXPIMP_TEMPLATE template class DECLSPECIFIER std::vector<glm::vec3>;
-EXPIMP_TEMPLATE template class DECLSPECIFIER std::vector<glm::vec4>;
 
 #else
 #define DllExport 
